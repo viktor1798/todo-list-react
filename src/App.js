5b09a6f6a1a36188d7b25cbded0581aa6ff05f29
viktor1798 <vktrsmsk1798@gmail.com>
2023-03-22 20:48:08 +0300

    removal of excess

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="App-header">
+        <h1>My Task</h1>
+      </header>
+      form
+    </div>
+  );
+}
+
+export default App;
